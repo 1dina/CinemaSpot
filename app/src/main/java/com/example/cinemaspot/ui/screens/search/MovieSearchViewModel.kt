868141b93c5212897a2d5cd857c5ac1ccdb2de7b
1dina <dinafadel69@gmail.com>
2025-03-26 06:33:34 +0200

    handle pagination in search screen

--- app/src/main/java/com/example/cinemaspot/ui/screens/search/MovieSearchViewModel.kt
+++ app/src/main/java/com/example/cinemaspot/ui/screens/search/MovieSearchViewModel.kt
@@ -25,6 +25,18 @@ class MovieSearchViewModel @Inject constructor(
     val resultList: StateFlow<List<Result>> = _resultList
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
+    private val _isLoadingAnotherPage = MutableStateFlow(false)
+    val isLoadingAnotherPage: StateFlow<Boolean> = _isLoadingAnotherPage
+    private var totalPage = 1
+    private var currentPage = 1
+
+    fun loadNextPage() {
+        if (!isLoading.value && currentPage < totalPage ) {
+            currentPage++
+            _isLoadingAnotherPage.value = true
+            searchForMovie(page = currentPage, query = _searchQuery.value)
+        }
+    }
 
     init {
         viewModelScope.launch {
@@ -46,25 +58,29 @@ class MovieSearchViewModel @Inject constructor(
 
     fun searchForMovie(query: String, page: Int = 1) {
         viewModelScope.launch(Dispatchers.IO) {
+            if (page==1)
             _isLoading.value = true
+            _resultList.value = emptyList()
             try {
                 val response = searchForMovieUseCase.searchMovie(query, page)
                 if (response.isSuccessful) {
-                    _isLoading.value = false
-                    _resultList.value = response.body()?.results!!
+                        totalPage = response.body()!!.total_pages
+                        _resultList.update {it -> it + response.body()!!.results }
+
                 } else {
-                    _isLoading.value = false
                     Log.e(
                         "Search for movie",
                         "Failed to fetch credits: ${response.errorBody()?.string()}"
                     )
                 }
             } catch (e: Exception) {
-                _isLoading.value = false
                 Log.e(
                     "Search for movie",
                     "Error Message: ${e.message}"
                 )
+            }finally {
+                _isLoading.value = false
+                _isLoadingAnotherPage.value = false
             }
         }
     }
--- app/src/main/java/com/example/cinemaspot/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/cinemaspot/ui/screens/search/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.example.cinemaspot.ui.screens.search
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -14,9 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -27,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -61,6 +65,8 @@ fun SearchScreen(
     val searchViewModel = hiltViewModel<MovieSearchViewModel>()
     val resultList by searchViewModel.resultList.collectAsState()
     val isLoading by searchViewModel.isLoading.collectAsState()
+    val isLoadingAnotherPage by searchViewModel.isLoadingAnotherPage.collectAsState()
+    val listState = rememberLazyListState()
     LaunchedEffect(inputText) {
         searchViewModel.updateQuery(inputText)
     }
@@ -113,10 +119,30 @@ fun SearchScreen(
             Spacer(modifier = modifier.size(24.dp))
             if (isLoading) LoadingIndicator()
             else
-                LazyColumn {
+                LazyColumn (state = listState) {
                     items(resultList.size) { index ->
                         ResultItem(result = resultList[index]) { route, id ->
                             onMovieClicked(route, id)
+                        }
+                        if (index == resultList.size - 1) {
+                            Log.e("SearchScreen", "Reached the end of the list")
+                            searchViewModel.loadNextPage()
+                        }
+                    }
+                    item {
+                        Log.d("SearchScreen", "Recomposing Loading Indicator: $isLoadingAnotherPage")
+                        if (isLoadingAnotherPage && !isLoading) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator(
+                                    color =White,
+                                    modifier = Modifier.size(32.dp)
+                                )
+                            }
                         }
                     }
                 }
